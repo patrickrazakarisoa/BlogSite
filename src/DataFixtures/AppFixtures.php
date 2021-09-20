f30316be24db4caf35ba24c2c6e1759eb870e493
kenjy <kenjyzakary@hotmail.com>
2021-09-20 10:18:02 +0200

    1- Création de la base de donnée et implantation des fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker;
+use App\Entity\User;
+use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $faker = Faker\Factory::create();
+        $users = [];
+
+        for ($i=0; $i < 50; $i++) {
+            $user = new User();
+            $user->setUsername($faker->name);
+            $user->setFirstname($faker->firstName());
+            $user->setLastname($faker->lastName());
+            $user->setEmail($faker->email);
+            $user->setPassword($faker->password());
+            $user->setCreatedAt(new \DateTime());
+            $manager->persist($user);
+            $users[] = $user;            
+        }
+
+        $categories = [];
+
+        for ($i=0; $i < 15; $i++) {
+            $category = new Category();
+            $category->setTitle($faker->text(50));
+            $category->setDescription($faker->text(250));
+            $category->setImage($faker->imageUrl());
+            $user->setEmail($faker->email);
+            $manager->persist($category);
+            $categories[] = $category;            
+        }
+
+        for ($i=0; $i < 100; $i++) {
+            $article = new Article();
+            $article->setTitle($faker->text(50));
+            $article->setContent($faker->text(5000));
+            $article->setImage($faker->imageUrl());
+            $article->setCreatedAt( new \DateTime());
+            $article->addCategory($categories[$faker->numberBetween(0,14)]);
+            $article->setAuthor($users[$faker->numberBetween(0,49)]);
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+}
